62f867896e7834448771569cdac6ad2fdc18545f
Nova <solarzn@lopers.unk.edu>
2024-09-30 16:39:50 -0500

    Monday in-class modified the "Checked" visualizer project

--- Week_5/Wednesday/scripts.js
+++ Week_5/Wednesday/scripts.js
@@ -1,6 +1,7 @@
 /*
-* 2024-09-25T16:40-5:00
-* Nova Solarz
+* 2024-09-25T16:40-05:00
+* UPDATE: 2024-09-30T16:45-05:00
+* Nova Solarz ( it / they / she )
 */
 
 /* --- MAIN -----------------------------------------------------------------------------*/
@@ -10,6 +11,9 @@ let rowsInput = document.getElementById("rows");
 let colsInput = document.getElementById("cols");
 let renderDiv = document.getElementById("render");
 let rangeInput = document.getElementById("rangeInp");
+let sizeInput = document.getElementById("sizeInput");
+let themeInput = document.getElementById("themeSelect");
+let selectedTheme = null;
 
 
 rowsInput.addEventListener("change", function(e) {
@@ -19,14 +23,21 @@ colsInput.addEventListener("change", function(e) {
     console.log(colsInput.value);
 });
 
-let renderInterval = setInterval(render, 1000);
+let renderInterval = setInterval(render, 500);
 rangeInput.addEventListener("change", function(e) {
     console.log(rangeInput.value);
     clearInterval(renderInterval);
     renderInterval = setInterval(render, rangeInput.value);
-
 });
 
+themeInput.addEventListener("change", function(e) {
+    console.log("THEME CHANGE ----------------------------")
+    console.log(e.target.options[e.target.selectedIndex].value);
+    selectedTheme = e.target.options[e.target.selectedIndex].value;
+});
+
+
+/* --- FUNCTIONS ----------------------------------------------------------------------*/
 const hsl_col_perc = (percent, start, end) => {
     const a = percent / 100,
         b = (end - start) * a,
@@ -34,33 +45,74 @@ const hsl_col_perc = (percent, start, end) => {
     return 'hsl(' + c + ', 100%, 50%)';
 }
 
-/* --- FUNCTIONS ----------------------------------------------------------------------*/
+function renderSquare(colIndex, colCount, randInt, rowIndex, rowCount) {
+    let element = document.createElement("div");
+    element.className = "square";
+    element.style.width = sizeInput.value + "px";
+    element.style.height = sizeInput.value + "px";
+
+    if(randInt >= colIndex) {
+        if (!selectedTheme) {
+            //    if theme is null
+            element.style.backgroundColor = "chartreuse"
+        } else if (selectedTheme === "rainbow") {
+            element.style.backgroundColor = hsl_col_perc((colIndex + 1) / colCount * 100, 0, 360);
+        } else if (selectedTheme === "opacity") {
+            element.style.backgroundColor = ((colIndex + 1) / colCount).toString();
+        } else if (selectedTheme === "heatmap") {
+            element.style.backgroundColor = hsl_col_perc(100 - randInt / colCount * 100, 0, 120);
+        }
+    }
+    return element;
+}
+
+function renderCheckbox(colIndex, colCount, randInt, rowIndex, rowCount) {
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = true;
+    checkbox.style.accentColor = "black";
+    checkbox.style.width = sizeInput.value + "px";
+    checkbox.style.height = sizeInput.value + "px";
+
+    if(randInt >= colIndex) {
+        if (!selectedTheme) {
+            //    if theme is null
+            checkbox.style.accentColor = "deeppink"
+        } else if (selectedTheme === "rainbow") {
+            checkbox.style.accentColor = hsl_col_perc((colIndex + 1) / colCount * 100, 0, 360);
+        } else if (selectedTheme === "opacity") {
+            checkbox.style.opacity = ((colIndex + 1) / colCount).toString();
+        } else if (selectedTheme === "heatmap") {
+            checkbox.style.accentColor = hsl_col_perc(100 - randInt / colCount * 100, 0, 120);
+        }
+    }
+    return checkbox;
+}
+
 function render() {
-    console.log("--- RENDER! ---");
-    console.log("rowsInput: " + rowsInput.value.toString());
-    console.log("colsInput: " + colsInput.value.toString());
-    console.log("rangeInput: " + rangeInput.value.toString());
-
+    // console.log("--- RENDER! ---");
+    // console.log("rowsInput: " + rowsInput.value.toString());
+    // console.log("colsInput: " + colsInput.value.toString());
+    // console.log("rangeInput: " + rangeInput.value.toString());
     renderDiv.innerHTML = "";
+    let colCount = colsInput.value;
+    let rowCount = rowsInput.value;
 
-    for (let rowIndex = 1; rowIndex <= rowsInput.value; rowIndex++) {
-        for (let colIndex = 1; colIndex <= colsInput.value; colIndex++) {
+    for (let rowIndex = 1; rowIndex <= rowCount; rowIndex++) {
+        let randInt = Math.floor(Math.random() * colCount);
+        for (let colIndex = 1; colIndex <= colCount; colIndex++) {
 
-            let checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            checkbox.checked = true;
-            checkbox.style.accentColor = "black";
-            renderDiv.appendChild(checkbox);
+            // render checkbox
+            // let element = renderCheckbox(colIndex, colCount, randInt, rowIndex, rowCount);
+            let element = renderSquare(colIndex, colCount, randInt, rowIndex, rowCount);
 
-            let randInt = Math.floor(Math.random()*colsInput.value);
-            if(randInt+1 >= colIndex) {
-                checkbox.style.accentColor = hsl_col_perc(100 - randInt / colIndex * 100, 0, 120);
-            }
+            renderDiv.appendChild(element);
         }
         renderDiv.appendChild(document.createElement("br"));
     }
 
 }
+
 
 /*
 copied from: https://canvas.unk.edu/courses/48785/discussion_topics/458921
