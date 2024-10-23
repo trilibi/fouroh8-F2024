ea71a51062bdccc6fbc018609e6626f6132ab013
NicolasMack99 <nicmack@gmx.de>
2024-10-23 16:40:50 -0500

    Week9

---
+++ Week_9/gasSpendOn.js
@@ -0,0 +1,16 @@
+vehicle cost
+distance traveled
+gas price
+
+total_distance = 240
+gas_price = 3.33
+vehicles = [
+    {
+        id: vehicle_1
+        gas_mileage: 8
+    }
+    {
+        id: vehicle_1
+        gas_mileage: 24
+    }
+]
