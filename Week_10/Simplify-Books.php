725d17ce37bfdac529efe416685c217d5e859da3
Nova <solarzn@lopers.unk.edu>
2024-10-31 13:35:52 -0500

    PHP Simplify Books it done :)

--- Week_10/Simplify-Books.php
+++ Week_10/Simplify-Books.php
@@ -2,29 +2,33 @@
  * Simplify Books with PHP
  * https://canvas.unk.edu/courses/48785/assignments/654621
  * Nova Solarz
- * 2024-10-30
+ * 2024-10-31
  -->
 
 <?php
+
 $results = file_get_contents('https://google.com/books.json');
 $books = json_decode($results);
-
 print_r($books->response->books[0]->title);
 
 $simple = [];
-$simple[] = 4; // same as array.push() in JS
+$simple[] = 4;
 print_r($simple);
 
-$simple_books = [];
-$books_length = count($books->response->books); // count() == length()
-echo $books_length;
+echo "\n---\n\n";
 
-// TODO: v
-// we want to return a simple array of books that includes title, id, author, and cover-image.
-for($i = 0; $i < $books_length; $i++) {
-    $book = $books->response->books[$i];
+$simple_books = [];
+$books_length = count($books->response->books);
+echo $books_length;
+// we want to return a simple array of books that includes the title, id, author and cover image
+for ($i=0; $i < $books_length; $i++) {
+    print_r($i);
+    $book =  $books->response->books[$i];
     $simple_books[] = [
-        'title' => $book->title
+        'title' => $book->title,
+        'id' => $book->id,
+        'author' => $book->author,
+        'cover_image' => $book->cover,
     ];
 }
 print_r($simple_books);
