f6dd6b388145b25fe904c11bbf7cdd636e7330d6
Jer Hermance <trilibi@gmail.com>
2024-11-25 10:51:24 -0600

    initial_final_bootstrap

---
+++ final_bootstrap/board.js
@@ -0,0 +1,36 @@
+function getCellClass(x, y) {
+  return (
+    "cell " +
+    (x % 2 === 0
+      ? y % 2 === 0
+        ? "even"
+        : "odd"
+      : y % 2 !== 0
+      ? "even"
+      : "odd")
+  );
+}
+
+export default function Board({ grid, width }) {
+  // console
+  return (
+    <div id="board">
+      <div style={{ width: width }}>
+        {grid.map((row, x) => (
+          <div className="row" key={"row_" + x}>
+            {row.map((col, y) => (
+              <div
+                className={getCellClass(x, y)}
+                title={x + "," + y}
+                key={"col_" + y}
+              >
+                {x},{y}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      {/* console */}
+    </div>
+  );
+}
