0108c488625fd3f6b06e22f33e3996f06c9c4d27
DylNorr1s <dylnorrisj@gmail.com>
2024-11-25 16:24:01 -0600

    Just followed along in class

--- final/final/core.js
+++ final/final/core.js
@@ -2,22 +2,31 @@ import Board from "./board";
 import Sidebar from "./sidebar";
 
 const App = () => {
-  const [name, setName] = React.useState("anon");
+  const defaultName = window.localStorage.getItem('my_name, anon')
+  console.log(defaultName);
+  const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
+  const [grid, setGrid] = React.useState([]);
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
-  const rows = 10;
-  const cols = 10;
-  const grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
-  console.log(grid);
+
+  React.useEffect(() => {
+    const rows = 10;
+    const cols = 10;
+    const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
+    setGrid(new_grid);
+}, []);
 
   return (
     <div id="app_root">
       <nav>
+        COD Zombies
         <h1>{name}</h1>
+        <input type="text" onInput={(e) => setName(e.target.value)}
+        value={name}/>
       </nav>
       <div id="main">
         <Sidebar />
-        <Board grid={grid} width="90%" />
+        <Board grid={grid} width="70%" />
       </div>
     </div>
   );
