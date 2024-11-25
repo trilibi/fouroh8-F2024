340e876ad7a3503dbd7d761fba787f7462928eaa
ReidenF <fowlerr@lopers.unk.edu>
2024-11-25 16:27:26 -0600

    final

--- final/core.js
+++ final/core.js
@@ -3,26 +3,17 @@ import Sidebar from "./sidebar";
 
 const App = () => {
   const [name, setName] = React.useState("anon");
-  const [grid,setGrid] = React.useState([]);
-
-
-
-
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
-
-  React.useEffect(function() {
-    const rows = 8;
-    const cols = 8;
-    const grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
-    setGrid(new_grid);
-  }, []);
+  const rows = 10;
+  const cols = 10;
+  const grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
+  console.log(grid);
 
   return (
     <div id="app_root">
       <nav>
         <h1>{name}</h1>
-        <input type="text" onInput={(e) => setName(e.target.value)}/>
       </nav>
       <div id="main">
         <Sidebar />
