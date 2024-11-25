58e1fab4a314e940cbe56785ff3296b74c4fe613
Carssorole <86818471+Carssorole@users.noreply.github.com>
2024-11-25 16:24:40 -0600

    Beginning Final Game

--- final_bootstrap/core.js
+++ final_bootstrap/core.js
@@ -2,22 +2,30 @@ import Board from "./board";
 import Sidebar from "./sidebar";
 
 const App = () => {
-  const [name, setName] = React.useState("anon");
+  const defaultName = window.localStorage.getItem('my_name');
+  const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
+  const [grid, setGrid] = React.useState([]);
 
+  React.useEffect(() => {
+    const rows = 10;
+    const cols = 10;
+    const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
+    setGrid(new_grid);
+  }, []);
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
-  const rows = 10;
-  const cols = 10;
-  const grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
-  console.log(grid);
 
   return (
     <div id="app_root">
       <nav>
         <h1>{name}</h1>
+        <input type="text" onInput={(e) => {
+          setName(e.target.value);
+          window.localStorage.setItem('my_name', e.target.value);
+        }} value={name}/>
       </nav>
       <div id="main">
         <Sidebar />
-        <Board grid={grid} width="90%" />
+        <Board grid={grid} width="50%" />
       </div>
     </div>
   );
