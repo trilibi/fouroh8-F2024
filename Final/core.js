0aa82478d3fb90128080c98446bcf7e50c460165
Nova <solarzn@lopers.unk.edu>
2024-11-26 12:23:13 -0600

    Final folder init mostly just getting the boilerplate set up

--- Final/core.js
+++ Final/core.js
@@ -2,25 +2,35 @@ import Board from "./board";
 import Sidebar from "./sidebar";
 
 const App = () => {
-  const [name, setName] = React.useState("anon");
+    const localName = window.localStorage.getItem('UserName');
+    const [name, setName] = React.useState(localName ? localName : 'test');
+    const [grid, setGrid] = React.useState([]);
 
-  // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
-  const rows = 10;
-  const cols = 10;
-  const grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
-  console.log(grid);
+    // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
 
-  return (
-    <div id="app_root">
-      <nav>
-        <h1>{name}</h1>
-      </nav>
-      <div id="main">
-        <Sidebar />
-        <Board grid={grid} width="90%" />
-      </div>
+    React.useEffect(function(){
+        const rows = 10;
+        const cols = 10;
+        const gridTemp = Array.from({ length: rows }, () => new Array(cols).fill([]));
+        setGrid(gridTemp);
+    }, []);
+
+    return (<div id="app_root">
+        <nav>
+                <label>
+                    <span style={{paddingRight: "10px"}}>Name :</span>
+                    <input type="text" onInput={(e) => {
+                        setName(e.target.value)
+                        window.localStorage.setItem("UserName", e.target.value);
+                    }} value={name} placeholder={"enter a name!"} />
+                </label>
+        </nav>
+        <div id="main">
+            <Sidebar/>
+            <Board grid={grid} width="90%" />
+        </div>
     </div>
-  );
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
