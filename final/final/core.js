ce4ae04cf976622e1790c9ce691beb90960ccc35
darkassassin557 <145591414+darkassassin557@users.noreply.github.com>
2024-11-26 19:25:21 -0600

    Final update 11/25/24

---
+++ final/final/core.js
@@ -0,0 +1,37 @@
+import Board from "./board";
+import Sidebar from "./sidebar";
+
+const App = () => {
+  const defaultName = window.localStorage.getItem('my_name');
+  const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
+  const [grid, setGrid] = React.useState([]);
+
+  // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
+  
+  React.useEffect(function() {
+  
+  const rows = 10;
+  const cols = 10;
+  const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
+  setGrid(new_grid);
+  console.log(new_grid);
+  },[]);
+
+  return (
+    <div id="app_root">
+      <nav>
+        Our Grid Game, this class is an easy A
+        <input type="text"  onInput={(e) => {
+          setName(e.target.value);
+          window.localStorage.setItem('my_name', e.target.value);
+        }} value={name}/>
+      </nav>
+      <div id="main">
+        <Sidebar />
+        <Board grid={grid} width="90%" />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
