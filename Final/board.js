b45e77e2b6b99e9ed4f003a24ab9e56b8ce0246a
red2922 <jinthavongsa1@gmail.com>
2024-12-02 16:31:19 -0600

    Added API call

--- Final/board.js
+++ Final/board.js
@@ -1,36 +1,43 @@
 function getCellClass(x, y) {
-    return (
-      "cell " +
-      (x % 2 === 0
-        ? y % 2 === 0
-          ? "even"
-          : "odd"
-        : y % 2 !== 0
+  return (
+    "cell " +
+    (x % 2 === 0
+      ? y % 2 === 0
         ? "even"
-        : "odd")
-    );
-  }
-  
-  export default function Board({ grid, width }) {
-    // console
-    return (
-      <div id="board">
-        <div style={{ width: width }}>
-          {grid.map((row, x) => (
-            <div className="row" key={"row_" + x}>
-              {row.map((col, y) => (
-                <div
-                  className={getCellClass(x, y)}
-                  title={x + "," + y}
-                  key={"col_" + y}
-                >
-                  {x},{y}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-        {/* console */}
+        : "odd"
+      : y % 2 !== 0
+      ? "even"
+      : "odd")
+  );
+}
+
+export default function Board({ grid, width, updatePosition }) {
+  const cellClicked = (e, x, y) => {
+    updatePosition(x, y);
+  };
+
+  // console
+  return (
+    <div id="board">
+      <div style={{ width: width }}>
+        {grid.map((row, x) => (
+          <div className="row" key={"row_" + x}>
+            {row.map((col, y) => (
+              <div
+                className={getCellClass(x, y)}
+                title={x + "," + y}
+                key={"col_" + y}
+                onClick={(e) => cellClicked(e, x, y)}
+                data-x={x}
+                data-y={y}
+              >
+                {x},{y}
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
-    );
-  }
+      {/* console */}
+    </div>
+  );
+}
--- Final/core.js
+++ Final/core.js
@@ -1,18 +1,32 @@
 import Board from "./board";
 import Sidebar from "./sidebar";
 
+const Pokedex = new window.Pokedex.Pokedex();
+
 const App = () => {
   // ?? null coalescing
   const defaultName = window.localStorage.getItem("name");
   const [name, setName] = React.useState(defaultName ? defaultName : "anon");
   const [grid, setGrid] = React.useState([]);
+  const [pokemonList, setPokemonList] = React.useState([]);
+  const [myPosition, setMyPosition] = React.useState({ x: 0, y: 0 });
+  const [myAvatar, setMyAvatar] = React.useState({ name: "", id: 0 });
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
   React.useEffect(() => {
     const rows = 10;
     const cols = 10;
     setGrid(Array.from({ length: rows }, () => new Array(cols).fill([])));
+
+    Pokedex.getPokemonsList().then(function (response) {
+      console.log(response);
+      setPokemonList(response.results);
+    });
   }, []);
+
+  const updatePosition = (x, y) => {
+    setMyPosition({ x: x, y: y });
+  };
 
   return (
     <div id="app_root">
@@ -26,10 +40,28 @@ const App = () => {
           placeholder="Please Enter a Name"
           title="Name Input"
         />
+        ({myPosition.x}, {myPosition.y}) (Avatar Name: {myAvatar.name}, Id:,
+        {myAvatar.id}) (Available Pokemon: {pokemonList.length});
       </nav>
+      {myAvatar.id == 0 && (
+        <div className="avatar-picker">
+          No Avatar:{" "}
+          {pokemonList.map(function (item) {
+            return (
+              <div
+                onClick={() => {
+                  console.log(item);
+                }}
+              >
+                {item.name}
+              </div>
+            );
+          })}
+        </div>
+      )}
       <div id="main">
         <Sidebar />
-        <Board grid={grid} width="90%" />
+        <Board grid={grid} width="90%" updatePosition={updatePosition} />
       </div>
     </div>
   );
