72f15aaf596abe29527012f5c0b3152492acaf2c
darkassassin557 <145591414+darkassassin557@users.noreply.github.com>
2024-12-02 16:14:49 -0600

    12/2/2024

--- final/final/core.js
+++ final/final/core.js
@@ -1,10 +1,29 @@
 import Board from "./board";
 import Sidebar from "./sidebar";
 
+const Pokedex = new window.Pokedex.Pokedex()
+const pokemon = [];
+
+Pokedex.getPokemonList()
+.then(function(response) {
+  console.log(response)
+  setPokemonList(response.results)
+})
+
+
 const App = () => {
   const defaultName = window.localStorage.getItem('my_name');
   const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
   const [grid, setGrid] = React.useState([]);
+  const [myPosition, setMyPosition] = React.userState({
+    y: 0,
+    y: 0
+  });
+
+  const [myAvatar, setMyAvatar] =React.userState({
+    name: '',
+    id: 0
+  });
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
   
@@ -17,14 +36,21 @@ const App = () => {
   console.log(new_grid);
   },[]);
 
+  function updatePosition(x,y) {
+    console.log('CORE :: ', x, y);
+    setMyPosition({x: x,y});
+  }
+
+
+
   return (
     <div id="app_root">
-      <nav>
+      <nav myPosition={myPosition}>
         Our Grid Game, this class is an easy A
         <input type="text"  onInput={(e) => {
           setName(e.target.value);
           window.localStorage.setItem('my_name', e.target.value);
-        }} value={name}/>
+        }} value={name}/> ({myPosition.x})
       </nav>
       <div id="main">
         <Sidebar />
