d40a69d67cf2e450f72e7aa7ced11a476ebdf2cd
DylNorr1s <dylnorrisj@gmail.com>
2024-12-02 16:30:40 -0600

    More in class

--- final/final/core.js
+++ final/final/core.js
@@ -1,11 +1,16 @@
 import Board from "./board";
 import Sidebar from "./sidebar";
 
+const Pokedex = new window.Pokedex.Pokedex()
+
 const App = () => {
   const defaultName = window.localStorage.getItem('my_name, anon')
   console.log(defaultName);
   const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
   const [grid, setGrid] = React.useState([]);
+  const [PokemonList, setPokemonList] = React.useState([]);
+  const [myPosition, setMyPosition] = React.useState({x:0, y:0});
+  const [myAvatar, setMyAvatar] = React.useState({name:'', id:0});
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
 
@@ -14,7 +19,17 @@ const App = () => {
     const cols = 10;
     const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
     setGrid(new_grid);
+
+    Pokedex.getPokemonsList()
+  .then(function(response) {
+    setPokemonList(response.results)
+  })
 }, []);
+
+function updatePosition(x, y) {
+  console.log('CORE :: ', x, y);
+  setMyPosition({x: x, y: y});
+}
 
   return (
     <div id="app_root">
@@ -24,11 +39,20 @@ const App = () => {
         <input type="text" onInput={(e) => {
           setName(e.target.value);
           window.localStorage.setItem('my_name', e.target.value)
-        }} value={name}/>
+        }} value={name}/> ({myPosition.x}, {myPosition.y}) 
+        (Avatar name{myAvatar.name}, ID# {myAvatar.id})
+        (Available Pokemon: {PokemonList.length})
       </nav>
+      {myAvatar.id == 0 && <div className="avatar-picker">No Avatar: 
+        {PokemonList.map(function(item){
+        return <div onClick={() => {
+          console.log(item);
+          }}>{item.name}</div>
+        })} </div>}
+
       <div id="main">
         <Sidebar />
-        <Board grid={grid} width="70%" />
+        <Board grid={grid} width="70%" updatePosition={updatePosition}/>
       </div>
     </div>
   );
