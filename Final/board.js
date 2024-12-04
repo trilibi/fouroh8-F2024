fae8cc3de5239f16ce12f6e8c42ac61631960bb9
Taehyun An <ateahyun@naver.com>
2024-12-04 16:24:53 -0600

    24-12-04 class

--- Final/board.js
+++ Final/board.js
@@ -1,36 +1,59 @@
 function getCellClass(x, y) {
-  return (
-    "cell " +
-    (x % 2 === 0
-      ? y % 2 === 0
-        ? "even"
-        : "odd"
-      : y % 2 !== 0
-      ? "even"
-      : "odd")
-  );
+    return (
+        "cell " +
+        (x % 2 === 0
+            ? y % 2 === 0
+                ? "even"
+                : "odd"
+            : y % 2 !== 0
+            ? "even"
+            : "odd")
+    )
 }
 
-export default function Board({ grid, width }) {
-  // console
-  return (
-    <div id="board">
-      <div style={{ width: width }}>
-        {grid.map((row, x) => (
-          <div className="row" key={"row_" + x}>
-            {row.map((col, y) => (
-              <div
-                className={getCellClass(x, y)}
-                title={x + "," + y}
-                key={"col_" + y}
-              >
-                {x},{y}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      {/* console */}
-    </div>
-  );
+export default function Board({
+    grid,
+    myAvatar,
+    myPosition,
+    width,
+    updatePosition,
+}) {
+    const cellClicked = (e, x, y) => {
+        updatePosition(x, y)
+    }
+    // console
+    return (
+        <div id="board">
+            <div style={{ width: width }}>
+                {grid.map((row, x) => (
+                    <div className="row" key={"row_" + x}>
+                        {row.map((col, y) => (
+                            <div
+                                className={getCellClass(x, y)}
+                                title={x + "," + y}
+                                key={"col_" + y}
+                                onClick={(e) => cellClicked(e, x, y)}
+                                data-x={x}
+                                data-y={y}
+                            >
+                                {/* {x},{y} */}
+                                {myPosition.x == x &&
+                                    myPosition.y == y &&
+                                    myAvatar.id != 0 && (
+                                        <img
+                                            src={
+                                                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/" +
+                                                myAvatar.id +
+                                                ".gif"
+                                            }
+                                        />
+                                    )}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            {/* console */}
+        </div>
+    )
 }
--- Final/core.js
+++ Final/core.js
@@ -1,12 +1,22 @@
 import Board from "./board"
 import Sidebar from "./sidebar"
 
+const Pokedex = new window.Pokedex.Pokedex()
+
 const App = () => {
     const [name, setName] = React.useState(
         window.localStorage.getItem("my_name") || "anon"
     )
     const [grid, setGrid] = React.useState([])
-
+    const [pokemonList, setPokemonList] = React.useState([])
+    const [myPosition, setMyPosition] = React.useState({
+        x: 0,
+        y: 0,
+    })
+    const [myAvatar, setMyAvatar] = React.useState({
+        name: "",
+        id: 0,
+    })
     // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
 
     React.useEffect(() => {
@@ -14,7 +24,21 @@ const App = () => {
         const cols = 10
         const g = Array.from({ length: rows }, () => new Array(cols).fill([]))
         setGrid(g)
+        Pokedex.getPokemonsList().then(function (response) {
+            let list = response.results
+            list = list.map((item) => {
+                console.log("https://pokeapi.co/api.v2.pokemon/".length)
+                item.id = item.url.slice(34, -1)
+                return item
+            })
+            setPokemonList(list)
+        })
     }, [])
+
+    function updatePosition(x, y) {
+        console.log("CORE :: ", x, y)
+        setMyPosition({ x: x, y: y })
+    }
 
     return (
         <div id="app_root">
@@ -28,10 +52,50 @@ const App = () => {
                     }}
                     value={name}
                 />
+                ({myPosition.x}, {myPosition.y}) (Avatar Name: {myAvatar.name},
+                id# {myAvatar.id})
+                <span
+                    onClick={() => {
+                        setMyAvatar({
+                            name: undefined,
+                            id: 0,
+                        })
+                    }}
+                >
+                    Clear Avatar
+                </span>
+                (Available pokemon: {pokemonList.length})
             </nav>
+            {myAvatar.id === 0 && (
+                <div className="avatar-picker">
+                    No Avatar:{" "}
+                    {pokemonList.map(function (item) {
+                        var baseURL =
+                            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"
+                        return (
+                            <img
+                                onClick={() => {
+                                    setMyAvatar({
+                                        name: item.name,
+                                        id: item.id,
+                                    })
+                                }}
+                                title={item.name}
+                                src={baseURL + item.id + ".gif"}
+                            />
+                        )
+                    })}
+                </div>
+            )}
             <div id="main">
                 <Sidebar />
-                <Board grid={grid} width="60%" />
+                <Board
+                    grid={grid}
+                    width="60%"
+                    myAvatar={myAvatar}
+                    myPosition={myPosition}
+                    updatePosition={updatePosition}
+                />
             </div>
         </div>
     )
