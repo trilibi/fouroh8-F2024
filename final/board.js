d9138274d5201535b75f209cfd2ee6c6c1dd58aa
Aoi Minamiyashiki <130811073+AoiMinamiyashiki@users.noreply.github.com>
2024-12-04 16:54:16 -0600

    class

--- final/board.js
+++ final/board.js
@@ -11,20 +11,37 @@ function getCellClass(x, y) {
   );
 }
 
-export default function Board({ grid, width }) {
+export default function Board({ grid, myAvatar, myPosition, width, updatePosition }) {
+
+  function cellClick(e, x, y) {
+    // console.log('clicked');
+    // console.log(e.target.dataset);
+    // console.log(e.target.dataset.x, e.target.dataset.y);
+    // console.log(x, y);
+    updatePosition(x, y);   
+  }
+
   // console
   return (
     <div id="board">
       <div style={{ width: width }}>
-        {grid.map((row, x) => (
+      {grid.map((row, x) => (
           <div className="row" key={"row_" + x}>
             {row.map((col, y) => (
               <div
                 className={getCellClass(x, y)}
                 title={x + "," + y}
                 key={"col_" + y}
+                onClick={(e) => cellClick(e, x,y)}
+                data-x={x}
+                data-y={y}
               >
-                {x},{y}
+
+                {myPosition.x == x && myPosition.y == y
+                && <img
+                src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'
+                  +myAvatar.id+'.gif'}
+                />}
               </div>
             ))}
           </div>
--- final/core.js
+++ final/core.js
@@ -1,10 +1,25 @@
 import Board from "./board";
 import Sidebar from "./sidebar";
 
+const Pokedex = new window.Pokedex.Pokedex();
+
+
+  
+
 const App = () => {
   const defaultName = window.localStorage.getItem('my_name');
   const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
   const [grid, setGrid] = React.useState([]);
+  const [pokemonList, setPokemonList] = React.useState([]);
+  const [myPosition, setMyposition] = React.useState({
+    x: 0,
+    y: 0
+  });
+
+  const [myAvatar, setMyAvatar] = React.useState({
+    name:'',
+    id: 0
+  });
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
 
@@ -13,7 +28,23 @@ const App = () => {
     const cols = 10;
     const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
     setGrid(new_grid);
+
+    Pokedex.getPokemonsList()
+    .then(function(response) {
+      console.log(response);
+      let list = response.results;
+      list = list.map((item) => {
+        item.id = item.url.slice(34, -1);
+        return item;
+      })
+      setPokemonList(list);
+    })
   }, []);
+
+  function updatePosition(x, y) {
+    console.log('CORE :: ', x, y);
+    setMyposition({x: x, y});
+  }
   
 
   return (
@@ -23,11 +54,37 @@ const App = () => {
         <input type="text" onInput={(e) => {
           setName(e.target.value);
           window.localStorage.setItem('my_name', e.target.value);
-        }} value={name} />
+        }} value={name} /> 
+        ({myPosition.x}, {myPosition.y})
+        (Avatar Name: {myAvatar.name}, id# {myAvatar.id})
+        <span onClick={() => {
+          setMyAvatar({name: '', id: 0});
+        }}>Clear Avatar</span>
+        (Available Pokemon: {pokemonList.length})
       </nav>
+
+      {myAvatar.id == 0 && 
+      <div className="avatar-picker">No Avatar: 
+        {pokemonList.map(function(item){
+          var baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated';
+          return (<img
+          onClick={() => { 
+            console.log(item); 
+            setMyAvatar({name: item.name, id: item.id});
+          }}
+          title = {item.name}
+          src={baseUrl + '/' + item.id + '.gif'} />);
+           
+      })}
+      </div>}
       <div id="main">
         <Sidebar />
-        <Board grid={grid} width="70%" />
+        <Board 
+        grid={grid} 
+        width="70%" 
+        myAvatar={myAvatar}
+        myPosition={myPosition}
+        updatePosition={updatePosition}/>
       </div>
     </div>
   );
