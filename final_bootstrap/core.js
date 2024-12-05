6e0b861a1392357a460e44ba03763df7607a0c4c
portilloi <portilloi@lopers.unk.edu>
2024-12-04 22:19:36 -0600

    Avatar icons

--- final_bootstrap/core.js
+++ final_bootstrap/core.js
@@ -1,23 +1,84 @@
 import Board from "./board";
 import Sidebar from "./sidebar";
 
+const Pokedex = new window.Pokedex.Pokedex()
+const pokemon = [];
+
+
+
 const App = () => {
-  const [name, setName] = React.useState("anon");
+  const defaultName = window.localStorage.getItem('my_name');
+  const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
+  const [grid, setGrid] = React.useState([]);
+  const [pokemonList, setPokemonList] = React.useState([]);
+  const [myPosition, setMyPosition] = React.useState({
+    x: 0,
+    y: 0,
+  });
+  const [myAvatar, setMyAvatar] = React.useState({
+    name: '',
+    id: 0,
+  });
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
-  const rows = 10;
-  const cols = 10;
-  const grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
-  console.log(grid);
+
+  React.useEffect(function () {
+    const rows = 10;
+    const cols = 10;
+    const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
+    setGrid(new_grid);
+
+    Pokedex.getPokemonsList()
+      .then(function (response) {
+        let list = response.results;
+        list = list.map((item) => {
+          item.id = item.url.slice(34, -1);
+          return item;
+        });
+        setPokemonList(list);
+      });
+  }, []);
+
+  function updatePosition(x, y) {
+    console.log("CORE :: ", x, y);
+    setMyPosition({ x, y });
+  }
 
   return (
     <div id="app_root">
       <nav>
         <h1>{name}</h1>
+        <input type="text" onInput={(e) => {
+          setName(e.target.value);
+          window.localStorage.setItem('my_name', e.target.value);
+        }} value={name} /> ({myPosition.x}, {myPosition.y})
+        (Avatar Name: {myAvatar.name}, id# {myAvatar.id})
+        <span onClick={() => {
+          setMyAvatar({name: '', id: 0});
+        }}>Clear Avatar</span>
+        (Available Pokemon: {pokemonList.length})
       </nav>
-      <div id="main">
+      {myAvatar.id == 0 && <div className="avatar-picker">No Avatar: {pokemonList.map(function (item){
+        var baseURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated"
+        return (<img
+          onClick={() => {
+            console.log(item);
+            setMyAvatar({name: item.name, id: item.id});
+          }
+          }
+          title={item.name}
+          src={baseURL + '/' + item.id + '.gif'}/>);
+      
+        })}
+        </div>}
+        < div id = "main" >
         <Sidebar />
-        <Board grid={grid} width="90%" />
+        <Board 
+          grid={grid} 
+          width="50%" 
+          myAvatar={myAvatar}
+          myPosition={myPosition}
+          updatePosition={updatePosition} />
       </div>
     </div>
   );
