a24448a416f6f23f47a16356f0df13d757a6e8a9
NicolasMack99 <nicmack@gmx.de>
2024-12-09 16:36:02 -0600

    Pokemon Project update

--- final/board.js
+++ final/board.js
@@ -11,7 +11,7 @@ function getCellClass(x, y) {
   );
 }
 
-export default function Board({ grid, myAvatar, myPosition, width, updatePosition }) {
+export default function Board({ grid, myAvatar, myPosition, width, updatePosition, avatars }) {
 
   function cellClicked(e, x, y){
     //console.log('clicked')
@@ -20,9 +20,11 @@ export default function Board({ grid, myAvatar, myPosition, width, updatePositio
     //console.log(x,y);
     updatePosition(x,y);
   }
+  
   // console
   return (
     <div id="board">
+      <pre>{JSON.stringify(avatars)}</pre>
       <div style={{ width: width }}>
         {grid.map((row, x) => (
           <div className="row" key={"row_" + x}>
@@ -35,8 +37,13 @@ export default function Board({ grid, myAvatar, myPosition, width, updatePositio
                 data-x={x}
                 data-y={y}
               >
-                {x},{y}
                 {myPosition.x == x && myPosition.y == y &&<img src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/' + myAvatar.id + '.gif'} />}
+
+                {Object.values(avatars).map((a) => {
+                  if (a.x === x && a.y === y) {
+                    return (<div><img src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/' + a.avatar.id + '.gif'} /></div>);
+                  }
+                })}
               </div>
             ))}
           </div>
