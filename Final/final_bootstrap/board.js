cd50bdb8a271dde61abdbf99401e9d983288cce7
Klayton Pagel <pagelk@lopers.unk.edu>
2024-12-09 13:03:47 -0600

    Work On Final

--- Final/final_bootstrap/board.js
+++ Final/final_bootstrap/board.js
@@ -1,36 +1,50 @@
 function getCellClass(x, y) {
-  return (
-    "cell " +
-    (x % 2 === 0
-      ? y % 2 === 0
-        ? "even"
-        : "odd"
-      : y % 2 !== 0
-      ? "even"
-      : "odd")
-  );
+    return (
+        "cell " +
+        (x % 2 === 0
+            ? y % 2 === 0
+                ? "even"
+                : "odd"
+            : y % 2 !== 0
+                ? "even"
+                : "odd")
+    );
 }
 
-export default function Board({ grid, width }) {
-  // console
-  return (
-    <div id="board">
-      <div style={{ width: width }}>
-        {grid.map((row, x) => (
-          <div className="row" key={"row_" + x}>
-            {row.map((col, y) => (
-              <div
-                className={getCellClass(x, y)}
-                title={x + "," + y}
-                key={"col_" + y}
-              >
-                {x},{y}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      {/* console */}
-    </div>
-  );
+export default function Board({ grid, myAvatar, myPosition, width, updatePosition }) {
+
+    function cellClicked(e) {
+        // console.log('clicked');
+        // console.log(e.target.dataset);
+        // console.log(e.target.dataset.x, e.target.dataset.y);
+        updatePosition(e.target.dataset.x, e.target.dataset.y);
+
+    }
+
+
+    // console
+    return (
+        <div id="board">
+            <div style={{ width: width }}>
+                {grid.map((row, x) => (
+                    <div className="row" key={"row_" + x}>
+                        {row.map((col, y) => (
+                            <div
+                                className={getCellClass(x, y)}
+                                title={x + "," + y}
+                                key={"col_" + y}
+                                onClick={cellClicked}
+                                data-x={x}
+                                data-y={y}
+                            >
+                                {myPosition.x == x && myPosition.y == y &&
+                                    <img src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'+myAvatar.id+'.gif'} />
+                                }</div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            {/* console */}
+        </div>
+    );
 }
--- Final/final_bootstrap/core.js
+++ Final/final_bootstrap/core.js
@@ -1,36 +1,88 @@
 import Board from "./board";
 import Sidebar from "./sidebar";
 
+const Pokedex = new window.Pokedex.Pokedex();
+const pokemon = [];
+
+
+
 const App = () => {
-  const defaultName = (window.localStorage.getItem('myName'));
-  const [name, setName] = React.useState(defaultName ? defaultName : 'secret');
-  const [grid, setgrid] = React.useState([]);
+    const defaultName = (window.localStorage.getItem('myName'));
+    const [name, setName] = React.useState(defaultName ? defaultName : 'secret');
+    const [grid, setgrid] = React.useState([]);
+    const [pokemonList, setPokemonList] = React.useState([]);
+    const [myPosition, setMyPosition] = React.useState({
+        x:0,
+        y:0
+    });
+    const [ myAvatar, setMyAvatar] = React.useState({
+        name:'',
+        id:0
+    });
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
 
-  React.useEffect(function() {
-    const rows = 10;
-    const cols = 10;
-    const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
-    setgrid(new_grid);
-  }, [])
-  
+    React.useEffect(function() {
+        const rows = 10;
+        const cols = 10;
+        const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
+        setgrid(new_grid);
 
-  return (
-    <div id="app_root">
-      <nav>
-        Grid Game
-        <input type="text" onInput={(e) => {
-            setName(e.target.value);
-            window.localStorage.setItem('myName', e.target.value);
-          }} value={name}/>
-      </nav>
-      <div id="main">
-        <Sidebar />
-        <Board grid={grid} width="90%" />
-      </div>
-    </div>
-  );
+        Pokedex.getPokemonsList()
+            .then(function(response) {
+                console.log(response);
+                let allPokemon = response.results;
+                allPokemon = allPokemon.map((item) => {
+                    item.id = item.url.slice(34, -1);
+                    return item;
+                })
+                setPokemonList(allPokemon);
+            });
+    }, [])
+
+    function updatePosition(x,y) {
+        console.log('CORE', x, y)
+        setMyPosition({x, y})
+    }
+
+    return (
+        <div id="app_root">
+            <nav>
+                Grid Game
+                <input type="text" onInput={(e) => {
+                    setName(e.target.value);
+                    window.localStorage.setItem('myName', e.target.value);
+                }} value={name}/>
+                ({myPosition.x}, {myPosition.y})
+                (Avatar Name: {myAvatar.name}, id# {myAvatar.id})
+                <span onClick={() => {
+                    setMyAvatar({name: '', id: 0});
+                }}>Clear Avatar</span>
+                (Available Pokemon: {pokemonList.length})
+            </nav>
+            {myAvatar.id === 0 && <div className="avatar-picker">NO AVATAR:
+                {pokemonList.map(function(item){
+                    var baseUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"
+                    return (<img
+                        onClick={()=> {
+                            console.log(item);
+                            setMyAvatar({name: item.name, id: item.id});
+                        }}
+                        title={item.name}
+                        src={baseUrl + '/' + item.id + '.gif'} />)
+
+                })}</div>}
+            <div id="main">
+                <Sidebar />
+                <Board
+                    grid={grid}
+                    width="90%"
+                    myAvatar={myAvatar}
+                    myPosition={myPosition}
+                    updatePosition={updatePosition} />
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
