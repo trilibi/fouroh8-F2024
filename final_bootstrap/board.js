17fa8e701f37b645e9d0c637beaa3094327f8415
portilloi <portilloi@lopers.unk.edu>
2024-12-11 16:09:24 -0600

    Chat

--- final_bootstrap/board.js
+++ final_bootstrap/board.js
@@ -11,35 +11,48 @@ function getCellClass(x, y) {
   );
 }
 
-export default function Board({ grid, width, myAvatar, myPosition, updatePosition }) {
+export default function Board({ grid, myAvatar, myPosition, width, updatePosition, avatars }) {
 
-  function cellClicked(e, x, y) {
-    console.log("clicked");
-    console.log(e.target.dataset);
-    console.log(e.target.dataset.x, e.target.dataset.y);
-    console.log(x, y);
-    updatePosition(x, y);
+  function cellClick(e, x, y) {
+    // console.log('clicked');
+    // console.log(e.target.dataset);
+    // console.log(e.target.dataset.x, e.target.dataset.y);
+    // console.log(x, y);
+    updatePosition(x, y);   
   }
+
   // console
   return (
     <div id="board">
+      <pre>{JSON.stringify(avatars)}</pre>
       <div style={{ width: width }}>
-        {grid.map((row, x) => (
+      {grid.map((row, x) => (
           <div className="row" key={"row_" + x}>
             {row.map((col, y) => (
               <div
                 className={getCellClass(x, y)}
                 title={x + "," + y}
                 key={"col_" + y}
-                onClick={(e) => cellClicked(e, x, y)}
+                onClick={(e) => cellClick(e, x,y)}
                 data-x={x}
                 data-y={y}
               >
-                {x},{y}
+
                 {myPosition.x == x && myPosition.y == y
-                &&<img 
-                src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/' + myAvatar.id + '.gif'}/>
-                }
+                && <img
+                src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'
+                  +myAvatar.id+'.gif'}
+                />}
+
+                {Object.values(avatars).map((a) => {
+                  if (a.x === x && a.y === y) {
+                    return (<div><img
+                    src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/gen'
+                      +a.avatar.id+'.gif'}/></div>
+                    );
+                  }
+                })}
+                
               </div>
             ))}
           </div>
