e6085fbf217e440d570682586937e2d21fd49e34
Adam <adamnelson1080@yahoo.com>
2024-12-17 21:05:18 -0600

    FinalProject

---
+++ Final Project/board.js
@@ -0,0 +1,65 @@
+
+function getCellClass(x, y) {
+    return (
+      "cell " +
+      (x % 2 === 0
+        ? y % 2 === 0
+          ? "even"
+          : "odd"
+        : y % 2 !== 0
+        ? "even"
+        : "odd")
+    );
+  }
+  
+  export default function Board({ grid, myAvatar, myPosition, width, updatePosition, avatars }) {
+  
+    function cellClick(e, x, y) {
+      // console.log('clicked');
+      // console.log(e.target.dataset);
+      // console.log(e.target.dataset.x, e.target.dataset.y);
+      // console.log(x, y);
+      updatePosition(x, y);   
+    }
+  
+    // console
+    return (
+      <div id="board">
+        <pre>{JSON.stringify(avatars)}</pre>
+        <div style={{ width: width }}>
+        {grid.map((row, x) => (
+            <div className="row" key={"row_" + x}>
+              {row.map((col, y) => (
+                <div
+                  className={getCellClass(x, y)}
+                  title={x + "," + y}
+                  key={"col_" + y}
+                  onClick={(e) => cellClick(e, x,y)}
+                  data-x={x}
+                  data-y={y}
+                >
+  
+                  {myPosition.x == x && myPosition.y == y
+                  && <img 
+                  src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'
+                    +myAvatar.id+'.gif'}
+                  />}
+  
+                  {Object.values(avatars).map((a) => {
+                    if (a.x === x && a.y === y) {
+                      return (<img 
+                      src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'
+                        +a.avatar.id+'.gif'}/>
+                      );
+                    }
+                  })}
+                  
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
---
+++ Final Project/core.js
@@ -0,0 +1,125 @@
+import Board from "./board";
+import Sidebar from "./sidebar";
+
+const Pokedex = new window.Pokedex.Pokedex();
+console.log(window.Pokedex);
+
+const socket = io('http://54.234.88.206:3405');
+
+function sendUpdate(data) {
+  if (data.name == 'anon') {
+    console.log('name');
+  }
+  console.log({data});
+  socket.emit('avatar', data);
+}
+  
+
+const App = () => {
+  const defaultName = window.localStorage.getItem('my_name');
+  const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
+  const [grid, setGrid] = React.useState([]);
+  const [pokemonList, setPokemonList] = React.useState([]);
+  const [myPosition, setMyposition] = React.useState({
+    x: 0,
+    y: 0
+  });
+
+  const [myAvatar, setMyAvatar] = React.useState({
+    name:'',
+    id: 0
+  });
+  const [avatars, setAvatars] = React.useState({});
+
+  React.useEffect(function() {
+    const rows = 10;
+    const cols = 10;
+    const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
+    setGrid(new_grid);
+
+    socket.on('avatar', function(data) {
+      console.log(data);
+      setAvatars(function(previous) {
+        previous[data.name] = data;
+        return previous;
+      })
+    });
+
+    
+
+    Pokedex.getPokemonsList()
+    .then(function(response) {
+      console.log(response);
+      let list = response.results;
+      list = list.map((item) => {
+        item.id = item.url.slice(34, -1);
+        return item;
+      })
+      setPokemonList(list);
+    })
+  }, []);
+
+  React.useEffect(() => {
+    sendUpdate({
+        name: name,
+        avatar: myAvatar,
+        x: myPosition.x,
+        y: myPosition.y
+      })
+  }, [myPosition]);
+
+  function updatePosition(x, y) {
+    console.log('CORE :: ', x, y);
+    setMyposition({x: x, y});
+  }
+  
+
+  return (
+    <div id="app_root">
+      <nav>
+        Grid Game
+        <input type="text" onInput={(e) => {
+          setName(e.target.value);
+          window.localStorage.setItem('my_name', e.target.value);
+        }} value={name} /> 
+        ({myPosition.x}, {myPosition.y})
+        (Avatar Name: {myAvatar.name}, id# {myAvatar.id})
+        <span onClick={() => {
+          setMyAvatar({name: '', id: 0});
+        }}>Clear Avatar</span>
+        (Available Pokemon: {pokemonList.length})
+      </nav>
+
+      {myAvatar.id == 0 && 
+      <div className="avatar-picker">No Avatar: 
+        {pokemonList.map(function(item){
+          var baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated';
+          return (<img
+          onClick={() => { 
+            console.log(item); 
+            setMyAvatar({name: item.name, id: item.id});
+          }}
+          title = {item.name}
+          src={baseUrl + '/' + item.id + '.gif'} />);
+           
+      })}
+      </div>}
+      <div id="main">
+        <Sidebar 
+          socket={socket}
+          name={name}
+          myAvatar={myAvatar}
+          myPosition={myPosition}/>
+        <Board 
+          grid={grid} 
+          width="70%" 
+          myAvatar={myAvatar}
+          avatars={avatars}
+          myPosition={myPosition}
+          updatePosition={updatePosition} />
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
