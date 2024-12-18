ca421d9670425b7f5d5fdbcb371723b0ff853b6f
Dreamclo <lattimorea@lopers.unk.edu>
2024-12-18 16:40:17 -0500

    Update sidebar.js

--- final/sidebar.js
+++ final/sidebar.js
@@ -1,13 +1,11 @@
 export default function Sidebar({socket, name, myAvatar, myPosition}) {
   const [messages, setMessages] = React.useState([]);
   const emoji = [
-      '🌟', '🥺', '🤣', '🤖', '🍕', '🎨', '💀', '👑', '💥', '😎',
-      '🍀', '🍩', '🎃', '💥', '🎯', '🦄', '👻', '🎸', '🍔', '🎅',
-      '💎', '🍎', '🔥', '🎮', '🦊', '🌵', '🥑', '🎁', '💝', '💀',
-      '🕺', '👩‍🍳', '🌍', '🪐', '🧸', '🤯', '🍓', '🎀', '🐶', '🧙‍♂️',
-      '👌', '🚑', '🍍', '🎲', '🚀', '🍦', '🌻', '🛸', '😁', '🧳'
-  ];
-  
+      '😂', '❤️', '🤣', '👍', '😭', '🙏', '😘', '🥰', '😍', '😊',
+      '🎉', '😁', '💕', '🥺', '😅', '🔥', '☺️', '🤦', '♥️', '🤷',
+      '🙄', '😆', '🤗', '😉', '🎂', '🤔', '👏', '🙂', '😳', '🥳',
+      '😎', '👌', '💜', '😔', '💪', '✨', '💖', '👀', '😋', '😏',
+      '😢', '👉', '💗', '😩', '💯', '🌹', '💞', '🎈', '💙', '😃'];
       
   React.useEffect(() => {
       socket.on('chat', (msg) => {
@@ -17,36 +15,27 @@ export default function Sidebar({socket, name, myAvatar, myPosition}) {
 
   function sendEmoji(em) {
       console.log(em);
-      setMessages(old => [...old, {name, em}]);
-      socket.emit('chat', {name: name, em: em, avatarID: myAvatar.id});
+
+      setMessages(old => [...old, {name, em, avatarId: myAvatar.id}]);
+      socket.emit('chat', {name: name, em: em, avatarId: myAvatar.id})
   }
 
   return <div id="sidebar">
-      {emoji.map(em => <span 
-          onClick={() => {
-              sendEmoji(em);
-          }}
-          className="emoji">
-          {em}
-      </span>)}
-      <hr />
-      {messages.reverse().map(obj => <div>
-          {obj.name}
-          {(() => {
-              if (obj.avatarID !== undefined && obj.avatarID !== 0) {
-                  return (
-                      <img 
-                          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${obj.avatarID}.gif`}
-                          alt={`Avatar ${obj.avatarID}`}
-                      />
-                  );
-              } else {
-                  return (
-                      <span role="img" aria-label="smile">😁</span>
-                  );
-              }
-          })()}
-          : {obj.em}
+    {emoji.map(em => <span 
+        onClick={() => {
+            sendEmoji(em);
+      }}
+      className="emoji">
+      {em}
+    </span>)}
+    <hr />
+    {messages.map(obj => <div>
+      {obj.name}
+      <img 
+        src={'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/'
+                  +obj.avatarId+'.gif'}
+                  />
+      : {obj.em}
       </div>)}
   </div>;
 }
