c25c611d4e4518bca55a31ec551291ba61a7a461
Bhavya Sharma <115432614+Bhavsszn@users.noreply.github.com>
2024-12-19 15:35:10 -0600

    Update core.js

--- Final/pokemon/core.js
+++ Final/pokemon/core.js
@@ -2,123 +2,87 @@ import Board from "./board";
 import Sidebar from "./sidebar";
 
 const Pokedex = new window.Pokedex.Pokedex();
-console.log(window.Pokedex);
-
-const socket = io('http://54.234.88.206:3405');
+const socket = io("http://54.234.88.206:3405");
 
 function sendUpdate(data) {
-  if (data.name == 'anon') {
-    console.log('name');
+  if (data.name === "anon") {
+    alert("Please set a name");
+    return;
   }
-  console.log({data});
-  socket.emit('avatar', data);
+  socket.emit("avatar", data);
 }
-  
 
 const App = () => {
-  const defaultName = window.localStorage.getItem('my_name');
-  const [name, setName] = React.useState(defaultName ? defaultName : 'anon');
+  const defaultName = window.localStorage.getItem("name");
+  const [name, setName] = React.useState(defaultName ? defaultName : "anon");
   const [grid, setGrid] = React.useState([]);
   const [pokemonList, setPokemonList] = React.useState([]);
-  const [myPosition, setMyposition] = React.useState({
-    x: 0,
-    y: 0
-  });
-
-  const [myAvatar, setMyAvatar] = React.useState({
-    name:'',
-    id: 0
-  });
+  const [myPosition, setMyPosition] = React.useState({ x: 0, y: 0 });
+  const [myAvatar, setMyAvatar] = React.useState({ name: "", id: 0 });
   const [avatars, setAvatars] = React.useState({});
 
   // https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
-
-  React.useEffect(function() {
+  React.useEffect(() => {
     const rows = 10;
     const cols = 10;
-    const new_grid = Array.from({ length: rows }, () => new Array(cols).fill([]));
-    setGrid(new_grid);
+    setGrid(Array.from({ length: rows }, () => new Array(cols).fill([])));
 
-    socket.on('avatar', function(data) {
-      console.log(data);
-      setAvatars(function(previous) {
-        previous[data.name] = data;
-        return previous;
-      })
+    socket.on("avatar", function (data) {
+      setAvatars((prevState) => {
+        prevState[data.name] = data;
+        return Object.assign({}, prevState);
+      });
     });
 
-    
-
-    Pokedex.getPokemonsList()
-    .then(function(response) {
-      console.log(response);
+    Pokedex.getPokemonsList().then(function (response) {
       let list = response.results;
       list = list.map((item) => {
         item.id = item.url.slice(34, -1);
         return item;
-      })
-      setPokemonList(list);
-    })
+      });
+      setPokemonList(response.results);
+    });
   }, []);
 
   React.useEffect(() => {
     sendUpdate({
-        name: name,
-        avatar: myAvatar,
-        x: myPosition.x,
-        y: myPosition.y
-      })
+      name: name,
+      avatar: myAvatar,
+      x: myPosition.x,
+      y: myPosition.y,
+    });
   }, [myPosition]);
 
-  function updatePosition(x, y) {
-    console.log('CORE :: ', x, y);
-    setMyposition({x: x, y});
-  }
-  
+  const updatePosition = (x, y) => {
+    setMyPosition({ x: x, y: y });
+  };
 
   return (
     <div id="app_root">
       <nav>
-        Grid Game
-        <input type="text" onInput={(e) => {
-          setName(e.target.value);
-          window.localStorage.setItem('my_name', e.target.value);
-        }} value={name} /> 
-        ({myPosition.x}, {myPosition.y})
-        (Avatar Name: {myAvatar.name}, id# {myAvatar.id})
-        <span onClick={() => {
-          setMyAvatar({name: '', id: 0});
-        }}>Clear Avatar</span>
-        (Available Pokemon: {pokemonList.length})
+        ({myPosition.x}, {myPosition.y}) (Avatar Name: {myAvatar.name}, Id:,
+        {myAvatar.id}) (Available Pokemon: {pokemonList.length});
       </nav>
 
-      {myAvatar.id == 0 && 
-      <div className="avatar-picker">No Avatar: 
-        {pokemonList.map(function(item){
-          var baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated';
-          return (<img
-          onClick={() => { 
-            console.log(item); 
-            setMyAvatar({name: item.name, id: item.id});
-          }}
-          title = {item.name}
-          src={baseUrl + '/' + item.id + '.gif'} />);
-           
-      })}
-      </div>}
       <div id="main">
-        <Sidebar 
+        <Sidebar
+          pokemonList={pokemonList}
+          setAvatarFunction={setMyAvatar}
+          myAvatar={myAvatar.id}
           socket={socket}
           name={name}
+          setName={setName}
+          setMyPosition={setMyPosition}
+        />
+        <Board
+          grid={grid}
+          width="90%"
+          updatePosition={updatePosition}
           myAvatar={myAvatar}
-          myPosition={myPosition}/>
-        <Board 
-          grid={grid} 
-          width="70%" 
-          myAvatar={myAvatar}
-          avatars={avatars}
           myPosition={myPosition}
-          updatePosition={updatePosition} />
+          avatars={avatars}
+          position={myPosition}
+        />
       </div>
     </div>
   );
